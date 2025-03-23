532533d7d71b184d691dd2b1f1a1f928cfd69b79
Michael Kasuku <michaelotienokasuku@gmail.com>
2025-03-23 20:17:47 +0300

    Images added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
+import { DiseaseDiagnosisComponent } from './disease-diagnosis/disease-diagnosis.component';
 import { HomeComponent } from './home/home.component';
 import { FarmerLoginComponent } from './farmer-login/farmer-login.component';
 import { FarmerSignupComponent } from './farmer-signup/farmer-signup.component';
-import { DiseaseDiagnosisComponent } from './disease-diagnosis/disease-diagnosis.component';
+import { FarmerForgotComponent } from './farmer-forgot/farmer-forgot.component';
 
 export const routes: Routes = [
+    { path: 'disease/diagnosis', component: DiseaseDiagnosisComponent },
     { path: '', component: HomeComponent },
     { path: 'farmer/login', component: FarmerLoginComponent },
     { path: 'farmer/signup', component: FarmerSignupComponent },
-    { path: 'disease/diagnosis', component: DiseaseDiagnosisComponent }
+    { path: 'farmer/forgot', component: FarmerForgotComponent },
 ];
--- src/app/farmer-forgot/farmer-forgot.component.ts
+++ src/app/farmer-forgot/farmer-forgot.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-farmer-forgot',
-  imports: [],
+  standalone:true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    FormsModule,
+  ],
   templateUrl: './farmer-forgot.component.html',
   styleUrl: './farmer-forgot.component.scss'
 })
 export class FarmerForgotComponent {
+  formData = {
+    Email: ''
+  };
 
+  constructor(
+    private snackBar: MatSnackBar,
+  ) { }
+
+  handlePasswordReset(event: Event) {
+    event.preventDefault();
+
+    // Basic Validation
+    if (!this.formData.Email) {
+      this.openSnackbar('Email address is required.', 'error');
+      return;
+    }
+  }
+
+  openSnackbar(message: string, severity: 'success' | 'error') {
+    const snackBarClass = severity === 'success' ? 'snackbar-success' : 'snackbar-error';
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      panelClass: snackBarClass,
+    });
+  }
+
+  closeSnackbar() {
+    this.snackBar.dismiss();
+  }
 }
